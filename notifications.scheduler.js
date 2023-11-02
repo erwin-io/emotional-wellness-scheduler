933e34f902bcc3ad104e70bf07c8b9083df9b00e
erwin-io <erwinramirez220@gmail.com>
2023-11-02 14:17:06 +0800

    last commit 2023-11-02

--- notifications.scheduler.js
+++ notifications.scheduler.js
@@ -1,17 +1,20 @@
 
 var request = require('request');
-if(process.env.NODE_ENV && process.env.NODE_ENV === "dev") {
-    require("dotenv").config({ path: "development.env"});
-}
-else {
-    require("dotenv").config();
-}
+// if(process.env.NODE_ENV && process.env.NODE_ENV === "dev") {
+//     require("dotenv").config({ path: "development.env"});
+// }
+// else {
+//     require("dotenv").config();
+// }
+
+require("dotenv").config({ path: "development.env"});
 
 const apiEndpoint = `${process.env.SCHEDULERAPIURL}/runReminder`;
 const post = async function() {
-    return await request.post(
+    return await request.get(
         apiEndpoint,
         async function (error, response, body) {
+            console.log("error", error);
             if (!error && response.statusCode == 200) {
                 return {
                     error,
@@ -23,5 +26,5 @@ const post = async function() {
     );
 };
 post().then(res => {
-    console.log('response ', res);
+    console.log('response ', res.error);
 })
